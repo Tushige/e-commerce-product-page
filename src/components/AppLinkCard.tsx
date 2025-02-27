d6740aebb347461d22d0a3723d0be9ae47cae4ac
Tushige <tushige2000@gmail.com>
2025-02-26 19:27:21 -0600

    fix text placement issue on mobile

--- src/components/AppLinkCard.tsx
+++ src/components/AppLinkCard.tsx
@@ -8,7 +8,12 @@ export default function AppLinkCard({ card }: AppLinkCardProps) {
   return (
     <div className="relative size-full lg:max-w-[32rem] rounded-xl overflow-hidden">
       <img src={imageUrl} className="size-full brightness-75" />
-      <div className="absolute w-full top-[50%] translate-y-[-50%] flex flex-col gap-2 text-white items-center justify-center p-2 lg:p-4">
+      <div
+        className="absolute w-full top-[50%] flex flex-col gap-2 text-white items-center justify-center p-2 lg:p-4"
+        style={{
+          transform: `translate(0, -50%)`,
+        }}
+      >
         <span className="font-miera-bold text-2xl lg:text-4xl">{title}</span>
         <span className="font-miera-demibold">{subtitle}</span>
         <AppButtonLink href={linkUrl}>{linkTitle}</AppButtonLink>
